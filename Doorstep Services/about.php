43da33f277cd14346adaa57c7a2692a83b12d8ce
Christin2001 <125427809+Christin2001@users.noreply.github.com>
2023-12-02 21:32:38 +0530

    Add files via upload

---
+++ Doorstep Services/about.php
@@ -0,0 +1,29 @@
+<?php include_once "./include/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <img src="images/ser.jpeg" class="col-4 card-img-top" alt="Logo">
+
+        <div class="container col-8" style="margin-top: 50px;">
+            <div class="card">
+                <div class="card-header text-center">
+                    <strong>DOORSTEP SERVICES SYSTEM</strong>
+                </div>
+
+                <div class="card-body">
+
+                <strong>Doorstep Services System is a website which  provide various services/service providers like plumber, electrician through online. The system is designed to handle some sort of services for all users. 
+                    The system will have ability to search various services which are listed in the system .The database will be updated automatically in a real time manner. This system is for finding necessary services and communicating with respective service provider registered.
+This website aims to use an online platform to introduce potential service providers to common people or users in a hyperlocal community, where users can view the service providers and can contact them either by sending a message or by calling them offline.
+</strong>
+
+            </div>
+
+            
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "./include/footers.php"; ?>
